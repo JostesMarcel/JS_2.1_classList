1fa1ecdb76ef708c3e132fb5942effde8656c887
JostesMarcel <jostes77@googlemail.com>
2022-01-19 13:36:30 +0100

    JS_2.1_classList

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function changeHeadline(){
+    document.getElementById("head").classList.add("headline")
+}
+
+function resetHeadline(){
+    document.getElementById("head").classList.remove("headline")
+}
+
+function changeList(){
+    document.getElementById("list").classList.add("liststyle")
+}
+
+function resetList(){
+    document.getElementById("list").classList.remove("liststyle")
+}
